d60fc4028d325b4719765bb53367f52e5f555cf5
waikayk <Kong_WaiKay@bah.com>
2018-09-29 00:56:02 -0400

    UI setup finished. Code about half done.

---
+++ SimpleRoster/Assets/Scripts/Entry.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+
+public class Entry : MonoBehaviour {
+	public InputField modelName;
+	public InputField points;
+	public InputField weapons;
+	public InputField specialism;
+	
+	public ToggleGroup experience;
+	public ToggleGroup fleshWounds;
+	
+	public Toggle convalescence;
+	public Toggle newRecruit;
+	
+	public string CreateJSON(){
+		Dictionary<string, string> payload = new Dictionary<string, string>();
+		payload.Add("modelName", modelName.text);
+		payload.Add("points", points.text);
+		payload.Add("weapons", weapons.text);
+		payload.Add("specialism", specialism.text);
+		
+		//TODO - Toggle stuff
+		
+		string json = JsonConvert.SerializeObject(payload);
+		
+		return json;
+	}
+	
+	public void TallyPoints(){
+		//
+	}
+}
---
+++ SimpleRoster/Assets/Scripts/MainController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+
+public class MainController : MonoBehaviour {
+	public Entry entryPrefab;
+	public RectTransform scrollContent;
+	
+	public InputField teamName;
+	
+	private List<Entry> entries;
+	
+	void Start(){
+		LoadRoster();
+	}
+	
+	public void AddEntry(){
+		Entry entry = Instantiate(entryPrefab) as Entry;
+		entry.transform.SetParent(scrollContent);
+		entry.transform.localScale = Vector3.one;
+		
+		entries.Add(entry);
+	}
+	
+	void AddEntry(Dictionary<string, string> entryInfo){
+		Entry entry = Instantiate(entryPrefab) as Entry;
+		entry.transform.SetParent(scrollContent);
+		entry.transform.localScale = Vector3.one;
+		
+		entry.modelName.text = entryInfo["modelName"];
+		entry.points.text = entryInfo["points"];
+		entry.weapons.text = entryInfo["weapons"];
+		entry.specialism.text = entryInfo["specialism"];
+		
+		entries.Add(entry);
+	}
+	
+	public void SaveRoster(){
+		List<string> payload = new List<string>();
+		foreach(Entry entry in entries){
+			payload.Add(entry.CreateJSON());
+		}
+		
+		string json = JsonConvert.SerializeObject(payload, Formatting.Indented);
+		
+		PlayerPrefs.SetString("teamName", teamName.text);
+		PlayerPrefs.SetString("roster", json);
+	}
+	
+	public void DeleteRoster(){
+		foreach(Entry entry in entries){
+			Destroy(entry.gameObject);
+		}
+		
+		entries = new List<Entry>();
+		
+		teamName.text = "";
+		
+		PlayerPrefs.DeleteKey("teamName");
+		PlayerPrefs.DeleteKey("roster");
+	}
+	
+	void LoadRoster(){
+		 entries = new List<Entry>();
+		
+		if(PlayerPrefs.HasKey("teamName")){
+			teamName.text = PlayerPrefs.GetString("teamName");
+		}
+		
+		if(PlayerPrefs.HasKey("roster")){
+			string json = PlayerPrefs.GetString("roster");
+			
+			List<string> load = JsonConvert.DeserializeObject<List<string>>(json);
+			
+			foreach(string entryJSON in load){
+				Dictionary<string, string> entryInfo = JsonConvert.DeserializeObject<Dictionary<string, string>>(entryJSON);
+				AddEntry(entryInfo);
+			}
+		}
+	}
+}
